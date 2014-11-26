7b9950ee70788fd7c58263149d74109f164f538d
MrSpijker <wannes.monchy@gmail.com>
2014-11-26 11:41:09 +0100

    index added to php

--- view/todos/overview.php
+++ view/todos/overview.php
@@ -1,67 +1,38 @@
-<div>
-    <div id="welcome">
-        <p><span class="name">Welcome <em><?php echo $_SESSION['user']['email'];?></em></span>
-            <span class="logout"><a href="index.php?page=logout">logout</a></span></p>
-    </div>
+	<div class="container">
 
-    <div id="addTodo">
-        <h2>Add new Todo</h2>
+		<audio preload="metadata">
 
-        <form method="post" action="index.php">
-            <fieldset>
-                <legend>add</legend>
-                <div>
-                    <label for="title">Title</label>
-                    <input class="<?php if(!empty($errors['title'])) echo 'has-error'; ?>" type="text" id="title" name="title"/>
-                    <span class="error-message<?php if(empty($errors['title'])) echo ' hidden';?>" data-for="title"><?php
-                    if(empty($errors['title'])) echo 'Please enter the title';
-                    else echo $errors['title'];
-                    ?></span>
-                </div>
-                <div>
-                    <label for="priority">Priority</label>
-                    <select name="priority" id="priority">
-                        <option value="1">Low Priority</option>
-                        <option value="2">Medium Priority</option>
-                        <option value="3">High Priority</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="submit" name="action" value="Add">
-                </div>
-            </fieldset>
-        </form>
+		</audio>
 
-    </div>
+		<header>
+			<h1>Blackboard</h1>
+		</header>
 
-    <div id="todoList">
+		<ul>
+			<form>
+				<li><input type="text" placeholder="name"></li>
+				<li><input type="text" placeholder="password"></li>
+				<li><input type="submit" value="submit" data-control="login"></li>
+				<li>or</li>
+				<li><input type="submit" value="register" data-control="register"></li>
+			</form>
+			<div class="user_controls">
+				<a href="#" class="control" data-control="add_project">new project</a>
+				<a href="#" class="control" data-control="invite_user">invite user</a>
+				<a href="#" class="control" data-control="save_project">save</a>
+				<a href="#" class="control" data-control="delete_project">delete</a>
 
-        <h2>Your Todo List</h2>
+		</div>
+			<div class="project_controls">
+				<a href="#" class="control" data-control="add_image">add image</a>
+				<a href="#" class="control" data-control="add_post-it">add post-it</a>
+				<a href="#" class="control" data-control="add_video">add video</a>
+		</div>
+		</ul>
 
-        <!-- voorbeeld, moet dynamisch worden.. -->
-        <ul>
-            <?php
-            
-            $priority = array(
-                '1' => 'Low',
-                '2' => 'Medium',
-                '3' => 'High');
-            if(!empty($todos)){
-                foreach($todos as $todo){
-                    echo "<li class='priority".$todo['priority']."' id='".$todo['id']."'>".$todo['title']." - ".$priority[$todo['priority']]." Priority
-                    <a class='delete' href='index.php?page=delete-todo&amp;id=".$todo['id']."'>done</a></li>";
-                }
-            }
-            else{
-                echo "No todos.";
-            }
-
-            ?>
-        </ul>
-
-        <!-- indien leeg -->
-
-        <!-- <p>[ no todos yet ]</p> -->
-
-    </div>
-</div>
+		<div class="indicators">
+			<div class="control-bar">
+				<div class="wrap">&nbsp;</div>
+			</div>
+		</div>
+	</div>
