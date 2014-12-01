6d254895b93eaebb844bf3c789b03f276eb07831
MrSpijker <SpijkeriMac@Wanness-iMac-2.local>
2014-12-01 11:10:26 +0100

    click events added

--- view/layout.php
+++ view/layout.php
@@ -16,7 +16,7 @@
 
     </body>
     <!--volgorde belangrijk -->
-    <script src="js/vendor/jquery.min.js"></script>
-    <script src="js/app.js"></script>
+    <script src="js/vendor/jquery-2.1.1.min.js"></script>
+    <script src="js/script.dist.js"></script>
 
 </html>
