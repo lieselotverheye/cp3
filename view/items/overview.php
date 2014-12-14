deeb488e7a9769fe23bc58fa7da1a9f8a8fea56c
MrSpijker <SpijkeriMac@Wanness-iMac-2.local>
2014-12-14 17:53:59 +0100

    post-it css made

--- view/items/overview.php
+++ view/items/overview.php
@@ -17,7 +17,16 @@
 		        	<header class="boardheader">
 		        		<h2>Project Title here</h2>
 		        		<p>users working on this project:</p>
-
+								<div class="postIt">
+									<select class="colors">
+  								<option value="cyaan">cyaan</option>
+								  <option value="magenta">magenta</option>
+								  <option value="yellow">yellow</option>
+								  <option value="key">key</option>
+								</select>
+								<p>padding-right: 5px; padding-right padding-right: 5px;padding-right: 5px;padding-right: 5px;padding-right: 5px;: 5px; padding-right: 5px; padding-right: 5px; padding-right: 5px;</p>
+		  						<input type="text" class="postIt-input" placeholder="type and press enter" />
+								</div>
 		        	</header>
 
             </div>
